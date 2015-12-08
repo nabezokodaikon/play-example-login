f52edbea45e3e9ab457ecf6005713cc6f1aa3eee
nabezokodaikon <nabezoko.daikon@gmail.com>
2015-12-09 03:40:07 +0900

    ヘルパーを使用したログイン処理を実装
    ヘルパーを使用しないログイン処理をコメントアウトで残してあるので、可能かどうか試してみる。

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,35 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
+import models.User
 
 class Application extends Controller {
 
-  def login = Action {
-    Ok(views.html.login())
+  val loginForm = Form(
+    mapping(
+      "email" -> text,
+      "password" -> text)(User.apply)(User.unapply))
+
+  def login() = Action {
+    val filledForm = loginForm.fill(User("E-mail Address", "Password"))
+    Ok(views.html.login(filledForm))
+  }
+
+  def auth() = Action { implicit request =>
+    loginForm.bindFromRequest.fold(
+      errors => {
+        println(errors)
+        Redirect(routes.Application.login())
+      },
+      success => {
+        println("Success!")
+        Ok(success.toString)
+      }
+    )
   }
 
 }
