2bf8e1bfa17cc488a4451c2f33fe1d065dada3c3
nabezokodaikon <nabezoko.daikon@gmail.com>
2015-12-11 01:25:16 +0900

    メアドとパスワードをダミーに変更

--- app/models/User.scala
+++ app/models/User.scala
@@ -5,7 +5,7 @@ case class User(email: String, password: String, role: Role)
 object User {
 
   def authenticate(email: String, password: String): Option[User] = {
-    if (email == """alienware.seven@gmail.com""" && password == """shinkuhadoken""") {
+    if (email == """example@gmail.com""" && password == """password""") {
       Some(User(email, password, Administrator))
     } else {
       None
